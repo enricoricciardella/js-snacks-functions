3da1875fbffedf95dedf28b78c9a1289568f3faf
enricoricciardella <enrico.ricciardella@gmail.com>
2024-11-06 16:38:51 +0100

    feat:1,2,3ok

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -13,21 +13,18 @@ const word = 'javascript';
 function contaVocali(stringa) {
     const vocali = ['a', 'e', 'i', 'o', 'u'];
     let conteggio = 0;
-
-    // Ciclo per contare le vocali
+    // Ciclo for per contare le vocali
     for (let i = 0; i < stringa.length; i++) {
         if (vocali.includes(stringa[i].toLowerCase())) {
             conteggio++;
-            // Incrementa il conteggio se il carattere è una vocale
         }
     }
-    return conteggio; // Ritorna il conteggio delle vocali
+    return conteggio; 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-const vocali = contaVocali(word); // Usa 'word' come parametro
-console.log(vocali); // Stampa il risultato (3)
 
-
+const vocali = contaVocali(word); 
+console.log(vocali);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
